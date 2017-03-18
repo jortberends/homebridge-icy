2f77e415631cf9f4a12f20f06eeac5c0e8f91651
Jort Berends <jort@appetite.nl>
2017-03-18 12:22:26 +0100

    Refactor to class

--- index.js
+++ index.js
@@ -7,38 +7,43 @@ module.exports = function(homebridge){
   homebridge.registerAccessory("homebridge-icy", "ICY", ICY);
 };
 
-function ICY(log, config) {
-	this.log = log;
-	this.maxTemp = config.maxTemp || 25;
-	this.minTemp = config.minTemp || 15;
-	this.name = config.name;
-	this.apiroute = config.apiroute || "apiroute";
-	this.log(this.name, this.apiroute);
-	this.username = config.username || null;
-	this.password = config.password || null;
-	this.token = null;
-	this.uid = null;
-
-	this.temperatureDisplayUnits = Characteristic.TemperatureDisplayUnits.CELSIUS;
-	this.currentTemperature = 19;
-	this.targetTemperature = 21;
-
-	this.service = new Service.Thermostat(this.name);
-
-}
-
-ICY.prototype = {
-	//Start
-	identify: function(callback) {
-		this.log("Identify requested!");
-		callback(null);
-	},
+class ICY {
+	constructor(log, config) {
+		this.log = log;
+		
+		this.name = config.name;
+		this.username = config.username;
+		this.password = config.password;
+		this.apiroute = config.apiroute;
+		
+		this.temperatureDisplayUnits = Characteristic.TemperatureDisplayUnits.CELSIUS;
+		this.temperature = 0;
+		this.targetTemperature = 0;
+		this.heatingThresholdTemperature = 0;
+		
+		this.uid = null;
+		this.token = null;
+		
+		this.heatingCoolingState = Characteristic.CurrentHeatingCoolingState.AUTO;
+		this.targetHeatingCoolingState = Characteristic.TargetHeatingCoolingState.AUTO;
+		
+		this.refreshToken((error) => {
+			if (error) {
+// 				this.log("Error refreshing token.");
+			} else {
+// 				this.log("Token loaded.");
+			}
+		});
+		
+		this.service = new Service.Thermostat(this.name);		
+	}
 	
-	getToken: function(callback) {
-		this.log("getToken");
+	refreshToken(callback) {
+// 		this.log("Receiving token.");
+		
 		if (this.token) {
 			callback(null, this.token);
-		} else {
+		} else {			
 			request.post({
 				url: this.apiroute+"/login",
 				form: {
@@ -47,64 +52,25 @@ ICY.prototype = {
 				}
 			}, function (err, response, body) {
 				if (!err && response.statusCode == 200) {
-					this.log("response success");
+// 					this.log("Login response success");
 					var json = JSON.parse(body);
 					this.token = json.token; // Login token
 					this.uid = json.serialthermostat1; // Thermostat serial, for setting temperature
-					this.log("token retrieved: " + this.token);
+// 					this.log("Login token retrieved: " + this.token);
 					callback(null, this.token);
 				} else {
+					this.log(error);
 					callback(err);				
 				}
 			}.bind(this));
 		}
-	},
+	}
 	
-	// Required
-	getCurrentHeatingCoolingState: function(callback) {
-		//	Not possible to implement for ICY
-		callback(null, Characteristic.CurrentHeatingCoolingState.OFF);
-	},
-	getTargetHeatingCoolingState: function(callback) {
-		//	Not possible to implement for ICY
-		callback(null, Characteristic.TargetHeatingCoolingState.OFF)
-	},
-	setTargetHeatingCoolingState: function(value, callback) {
-		//	Not possible to implement for ICY
-		callback(null);
-	},
-	getCurrentTemperature: function(callback) {
+	refreshThermostat(callback) {
+// 		this.log("Refreshing thermostat");
 		
-		this.log("getCurrentTemperature");
-		getToken(function (err, token) {
-			if (!err && token != null) {
-				request.get({
-					url: this.apiroute + "/data",
-					form: {
-						username: this.username,
-						password: this.password
-					}
-				}, function (err, response, body) {
-					if (!err && response.statusCode == 200) {
-						this.log("response success");
-						var json = JSON.parse(body);
-						this.currentTemperature = parseFloat(json.temperature2);
-						this.targetTemperature = parseFloat(json.temperature1);
-						callback(null, this.currentTemperature);
-					} else {
-						this.token = null;
-						callback(err);
-					}
-				}.bind(this));
-			} else {
-				this.log("Error retrieving token %s", err);
-			}
-		});
-	},
-	getTargetTemperature: function(callback) {
-		this.log("getTargetTemperature");
-		getToken(function (err, token) {
-			if (!err && token != null) {
+		this.refreshToken(function (error, token) {
+			if (token) {
 				request.get({
 					url: this.apiroute + "/data",
 					form: {
@@ -116,26 +82,71 @@ ICY.prototype = {
 					}
 				}, function (err, response, body) {
 					if (!err && response.statusCode == 200) {
-						this.log("response success");
+// 						this.log("Refresh thermostat response success");
 						var json = JSON.parse(body);
-						this.currentTemperature = parseFloat(json.temperature2);
+						this.temperature = parseFloat(json.temperature2);
 						this.targetTemperature = parseFloat(json.temperature1);
-						callback(null, this.targetTemperature);
+						callback(null);
 					} else {
+// 						this.log("Refreshing failed with body: ", body);
 						this.token = null;
+						this.log(error);
 						callback(err);
 					}
 				}.bind(this));
 			} else {
-				this.log("Error retrieving token %s", err);
+				callback(error);
 			}
-		});
-	},
-	setTargetTemperature: function(value, callback) {
-		this.log("setTargetTemperature");
-		getToken(function (err, token) {
-			if (!err && token != null) {
-				request.post({
+		}.bind(this));
+	}
+	
+    getCurrentHeatingCoolingState(callback) {
+//         this.log('getCurrentHeatingCoolingState:', this.heatingCoolingState);
+        callback(null, this.heatingCoolingState);
+    }
+	
+	getTargetHeatingCoolingState(callback) {
+//         this.log('getTargetHeatingCoolingState:', this.targetHeatingCoolingState);
+        callback(null, this.targetHeatingCoolingState);
+    }
+	
+    setTargetHeatingCoolingState(value, callback) {
+// 	    this.log("setTargetHeatingCoolingState: ",value);
+	    callback();
+    }
+	
+	getCurrentTemperature(callback) {
+// 		this.log("getCurrentTemperature");
+		this.refreshThermostat(function (error) {
+			if (error) {
+				this.log(error);
+				callback(error);
+			} else {
+// 				this.log("currentTemperature: ", this.temperature);
+				callback(null, this.temperature);
+			}
+		}.bind(this));
+	}
+	
+    getTargetTemperature(callback) {
+// 	    this.log("getTargetTemperature");
+		this.refreshThermostat(function (error) {
+			if (error) {
+				this.log(error);
+				callback(error);
+			} else {
+				this.log("targetTemperature: ", this.targetTemperature);
+				callback(null, this.targetTemperature);
+			}
+		}.bind(this));
+    }
+	
+	setTargetTemperature(value, callback) {
+//         this.log("setTargetTemperature: ", value);
+		
+		this.refreshToken(function (error, token) {
+			if (token) {
+		        request.post({
 					url: this.apiroute + "/data",
 					form: {
 						uid: this.uid,
@@ -146,84 +157,82 @@ ICY.prototype = {
 					}
 				}, function (err, response, body) {
 					if (!err && response.statusCode == 200) {
-						this.log("response success");
+// 						this.log("setTargetTemperature response success");
 						callback(null);
 					} else {
+						this.log(error);
 						this.token = null;
 						callback(err);
 					}
 				}.bind(this));
 			} else {
-				this.log("Error retrieving token %s", err);
+				callback(error);
 			}
-		});
-	},
-	getTemperatureDisplayUnits: function(callback) {
-		this.log("getTemperatureDisplayUnits:", this.temperatureDisplayUnits);
-		var error = null;
-		callback(error, this.temperatureDisplayUnits);
-	},
-	setTemperatureDisplayUnits: function(value, callback) {
-		this.log("setTemperatureDisplayUnits from %s to %s", this.temperatureDisplayUnits, value);
-		this.temperatureDisplayUnits = value;
-		var error = null;
-		callback(error);
-	},
-	getName: function(callback) {
-		this.log("getName :", this.name);
+		}.bind(this));
+    }
+	
+    getTemperatureDisplayUnits(callback) {
+//         this.log("getTemperatureDisplayUnits: ", this.temperatureDisplayUnits);
+        var error = null;
+        callback(error, this.temperatureDisplayUnits);
+    }
+	
+    setTemperatureDisplayUnits(value, callback) {
+//         this.log("setTemperatureDisplayUnits from %s to %s", this.temperatureDisplayUnits, value);
+        this.temperatureDisplayUnits = value;
+        var error = null;
+        callback(error);
+    }
+	
+    getHeatingThresholdTemperature(callback) {
+//         this.log("getHeatingThresholdTemperature: " , this.heatingThresholdTemperature);
+        var error = null;
+        callback(error, this.heatingThresholdTemperature);
+    }
+	
+	getName(callback) {
+// 		this.log("getName: ", this.name);
 		var error = null;
 		callback(error, this.name);
-	},
-
-	getServices: function() {
+	}
+	
+	getServices() {
+// 		this.log("Setting up services.");
+		
 		var informationService = new Service.AccessoryInformation();
-
+		
 		informationService
-			.setCharacteristic(Characteristic.Manufacturer, "HTTP Manufacturer")
-			.setCharacteristic(Characteristic.Model, "HTTP Model")
-			.setCharacteristic(Characteristic.SerialNumber, "HTTP Serial Number");
-
-		// Required Characteristics
-		this.service
-			.getCharacteristic(Characteristic.CurrentHeatingCoolingState)
-			.on('get', this.getCurrentHeatingCoolingState.bind(this));
-
+			.setCharacteristic(Characteristic.Manufacturer, 'Essent')
+			.setCharacteristic(Characteristic.Model, 'E-Thermostaat')
+			.setCharacteristic(Characteristic.SerialNumber, '');
+			
 		this.service
 			.getCharacteristic(Characteristic.TargetHeatingCoolingState)
 			.on('get', this.getTargetHeatingCoolingState.bind(this))
 			.on('set', this.setTargetHeatingCoolingState.bind(this));
-
-		this.service
-			.getCharacteristic(Characteristic.CurrentTemperature)
-			.on('get', this.getCurrentTemperature.bind(this));
-
-		this.service
-			.getCharacteristic(Characteristic.TargetTemperature)
-			.on('get', this.getTargetTemperature.bind(this))
-			.on('set', this.setTargetTemperature.bind(this));
-
-		this.service
-			.getCharacteristic(Characteristic.TemperatureDisplayUnits)
-			.on('get', this.getTemperatureDisplayUnits.bind(this))
-			.on('set', this.setTemperatureDisplayUnits.bind(this));
-
-		// Optional Characteristics
-		this.service
-			.getCharacteristic(Characteristic.Name)
-			.on('get', this.getName.bind(this));
 			
-		this.service.getCharacteristic(Characteristic.CurrentTemperature)
-			.setProps({
-				minValue: this.minTemp,
-				maxValue: this.maxTemp,
-				minStep: 1
-			});
-		this.service.getCharacteristic(Characteristic.TargetTemperature)
-			.setProps({
-				minValue: this.minTemp,
-				maxValue: this.maxTemp,
-				minStep: 1
-			});
-		return [informationService, this.service];
+        this.service
+            .getCharacteristic(Characteristic.CurrentTemperature)
+            .on('get', this.getCurrentTemperature.bind(this));
+
+        this.service
+            .getCharacteristic(Characteristic.TargetTemperature)
+            .on('get', this.getTargetTemperature.bind(this))
+            .on('set', this.setTargetTemperature.bind(this));
+
+        this.service
+            .getCharacteristic(Characteristic.TemperatureDisplayUnits)
+            .on('get', this.getTemperatureDisplayUnits.bind(this))
+            .on('set', this.setTemperatureDisplayUnits.bind(this));
+
+        this.service
+            .getCharacteristic(Characteristic.HeatingThresholdTemperature)
+            .on('get', this.getHeatingThresholdTemperature.bind(this));
+
+        this.service
+            .getCharacteristic(Characteristic.Name)
+            .on('get', this.getName.bind(this));
+            
+        return [informationService, this.service];
 	}
-};
+}
